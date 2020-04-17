6f757aef92a648a3533d2defdcbf6c9f76adaefb
Kruss <as704245@iutnice.unice.fr>
2020-04-17 14:53:56 +0200

    joueur

---
+++ src/main/Joueur.java
@@ -0,0 +1,28 @@
+package main;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Joueur {
+	private Map<String, Integer> stats = new HashMap<>();
+	private GestionnaireEquipements gestionnaireEquipements;
+	
+	//shadows, hunters ou neutre
+	public String getEquipe() {
+		return null;
+	}
+	
+	public int getHP() {
+		return stats.get("HP");
+	}
+	
+	public int getDamage() {
+		return stats.get("DAMAGE");
+	}
+	
+	public int getNbEquipments() {
+		return gestionnaireEquipements.getNbEquipments();
+	}
+	
+	
+
+}
