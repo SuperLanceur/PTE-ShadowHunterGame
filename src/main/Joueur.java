4d5a449a4c7147ebfb434492dfb7ad3dded728f7
herauda <59832812+herauda@users.noreply.github.com>
2020-04-17 16:04:44 +0200

    Add files via upload

--- src/main/Joueur.java
+++ src/main/Joueur.java
@@ -4,8 +4,44 @@ import java.util.List;
 import java.util.Map;
 
 public class Joueur {
-	private Map<String, Integer> stats = new HashMap<>();
 	private GestionnaireEquipements gestionnaireEquipements;
+	
+	private String name;
+	private boolean revealed;
+	private boolean virtual;
+	
+	// map keys
+	public static final String PLAYER_HP = "hp";
+	public static final String PLAYER_TURN = "turn";
+	public static final String PLYAYER_DAMAGE = "damage";
+	public static final String PLAYER_RESISTANCE = "resistance";
+	public static final String PLAYER_REVEAL = "reveal";
+	public static final String PLAYER_IMMUNITY = "immunity";
+	
+	private Map<String, Integer> stats;
+	
+	public Joueur(String name) {
+		this.name = name;
+		this.revealed = false;
+		this.virtual = false;
+		
+		stats = new HashMap<>();
+		
+		
+		// Initialisation joueur depuis valeurs perso
+		
+		//stats.put(PLAYER_HP, char.getHP());
+		//stats.put(PLAYER_TURN, 1);		
+		//stats.put(PLAYER_DAMAGE, 0); - dégats en +
+		//stats.put(PLAYER_RESISTANCE, 0);
+		//stats.put(PLAYER_REVEAL, 0);
+		//stats.put(PLAYER_IMMUNITY, 0); 
+		// immunité à certains effets?
+	}
+	
+	public void setVirtual() {
+		this.virtual = true;
+	}
 	
 	//shadows, hunters ou neutre
 	public String getEquipe() {
@@ -29,4 +65,7 @@ public class Joueur {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
+
 }
