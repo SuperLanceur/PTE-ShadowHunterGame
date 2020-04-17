2f99644369756180ea117cc5b3c49b11e2298e2c
Chiara <chiara.relevat@oulook.fr>
2020-04-17 15:16:08 +0200

    changement qui marche

--- src/ihm/controller/MenuController.java
+++ src/ihm/controller/MenuController.java
@@ -1,22 +1,33 @@
 package ihm.controller;
 
-import ihm.FxmlLoader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class MenuController {
-	private Stage window;
+public class MenuController implements Initializable{
+	@FXML
+	private Pane rootPane;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 
-   public void commencerPartie(MouseEvent mouseEvent) {
+	@FXML
+	public void commencerPartie(MouseEvent mouseEvent) throws IOException{
         System.out.println("Bonjouuuuur!");
-        FxmlLoader object = new FxmlLoader();
-        Pane view = object.getPage("Choix_joueur");
-        Scene scene = new Scene(view);
-        window.setScene(scene);
+        BorderPane pane = FXMLLoader.load(getClass().getResource("../ressources/Choix_joueur.fxml"));
+        System.out.println(rootPane);
+        rootPane.getChildren().setAll(pane);
     }
     
     public void afficherRegle(MouseEvent mouseEvent) {
