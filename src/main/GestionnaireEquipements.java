0afb08699573cb6228211bf388c96f7724db10d0
AhmedSaad <saad-el-din.ahmed@etu.unice.fr>
2020-04-21 18:01:18 +0200

    GestionnaireEquipements, voir methode pour les effets

--- src/main/GestionnaireEquipements.java
+++ src/main/GestionnaireEquipements.java
@@ -1,12 +1,51 @@
 package main;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import condition.Condition;
+
 public class GestionnaireEquipements {
-	private Map<String, Integer> stats = new HashMap<>();
-	
+	// String est le nom de l'equip, et Equip c'est l'objet, on suppose ici qu'il ne
+	// peut pas y avoir de doublon d'equipement
+	private Map<String, Equipement> equipements = new HashMap<>();
+
 	public int getNbEquipments() {
-		return stats.size();
+		return equipements.size();
 	}
+
+	// precondition : verifier que le gestionnaire contient l'equipement avec
+	// containsEquipement
+	public Equipement getEquipement(String key) {
+		return equipements.get(key);
+	}
+
+	public boolean containsEquipement(String key) {
+		return equipements.containsKey(key);
+	}
+
+	public void addEquipement(String s, Equipement e) {
+		equipements.put(s, e);
+	}
+
+	// ex : si le joueur se fait voler un equipement, on l'enleve de la liste
+	// d'equipements
+	public Equipement removeEquipement(String key) {
+		return equipements.remove(key);
+	}
+
+	// methode qui renvoit une liste des effets des equipements du joueur qui
+	// s'activeront dans une certaine phase de la partie par rapport a la condition
+	// en parametre
+	public List<Effet> getEffets(Condition c) {
+		// TODO
+		return null;
+	}
+
+	// pour methode choisir de joueur
+	public Equipement[] getArrayEquipements() {
+		return (Equipement[]) equipements.values().toArray();
+	}
+
 }
