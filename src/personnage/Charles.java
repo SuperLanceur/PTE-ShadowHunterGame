2698b4882327e935a336036edeb5b079ff346b5f
Johann <raptops06@hotmail.fr>
2020-04-21 15:09:01 +0200

    personnage Charles effectué

--- src/personnage/Charles.java
+++ src/personnage/Charles.java
@@ -1,5 +1,9 @@
 package personnage;
 
+import effet.Effet;
+import effet.EffetSelf;
+import effet.action.Action;
+import effet.action.ActionAltererStatistiquesJoueur;
 import main.Joueur;
 
 public class Charles extends CartePersonnage{
@@ -7,16 +11,25 @@ public class Charles extends CartePersonnage{
 	//constructeur
 	public Charles(String nom, int hp, Joueur joueur) {
 		super(nom, hp, joueur);
+		
+		Action action = new ActionAltererStatistiquesJoueur("HP",2,true);
+		Effet effet = new EffetSelf(action);
+		this.setEffet(effet);
 	}
 
 	//m�thode
 	public void attaquer(Joueur j) {
-		
+		super.attaquer(j);
+		if(this.getJoueur().getRevele())
+		{
+			utiliser(this.getJoueur());
+			super.attaquer(j);
+		}	
 	}
 
 	@Override
 	public void utiliser() {
 		// TODO Auto-generated method stub
-		
+		this.getEffet().utiliser(this.getJoueur());
 	}
 }
