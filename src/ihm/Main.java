249c9419621fddb76383c5972629abe1ba49a52b
Bastien NOEL <bastien.noel@hotmail.fr>
2020-04-24 14:21:26 +0200

    ajout d'une musique

--- src/ihm/Main.java
+++ src/ihm/Main.java
@@ -1,6 +1,5 @@
 package ihm;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,27 +7,26 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
 	@Override
-    public void start(Stage primaryStage) throws Exception{
+	public void start(Stage primaryStage) throws Exception {
 		System.out.println("Lancement de l'application");
-<<<<<<< HEAD
-        Pane root = FXMLLoader.load(getClass().getResource("ressources/Menu.fxml")); // "ressources/Jouer_tour(1)lancer_des.fxml"
-=======
-        Pane root = FXMLLoader.load(getClass().getResource("ressources/Menu.fxml"));
->>>>>>> 962e4e4abd757404ab08839e67817494b857931a
-        primaryStage.setTitle("Shadow Hunters");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.centerOnScreen();
-        primaryStage.show();
-    }
 
-    public static void main(String[] args) {
-    	
-    	String filepath = "src//ihm//ressources//musique//The_Red_Fox_Tavern.wav"; //lien vers la musique : https://www.youtube.com/watch?v=LBpKUIyOHdo
-    	Musique musiqueObjet = new Musique();
-    	musiqueObjet.playMusique(filepath);
-        launch(args);
-        
-    }
+		Pane root = FXMLLoader.load(getClass().getResource("ressources/Menu.fxml")); // "ressources/Jouer_tour(1)lancer_des.fxml"
+
+		primaryStage.setTitle("Shadow Hunters");
+		primaryStage.setScene(new Scene(root));
+		primaryStage.centerOnScreen();
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+
+		String filepath = "src//ihm//ressources//musique//The_Red_Fox_Tavern.wav"; // lien vers la musique :
+																					// https://www.youtube.com/watch?v=LBpKUIyOHdo
+		Musique musiqueObjet = new Musique();
+		musiqueObjet.playMusique(filepath);
+		launch(args);
+
+	}
 }
