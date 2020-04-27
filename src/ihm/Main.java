8c55091fb2b460776beaee791d7f1c9b58516419
Chiara <chiara.relevat@oulook.fr>
2020-04-27 16:53:32 +0200

    changement des

--- src/ihm/Main.java
+++ src/ihm/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		System.out.println("Lancement de l'application");
 		
-		 final URL fxmlURL = getClass().getResource("ressources/Menu.fxml");  // "ressources/Jouer_tour(1)lancer_des.fxml"
+		 final URL fxmlURL = getClass().getResource("ressources/Jouer_tour(1)lancer_des.fxml");  // "ressources/Jouer_tour(1)lancer_des.fxml"
 	     final ResourceBundle bundle = ResourceBundle.getBundle("domaine.properties.langue", Locale.FRANCE);
 	     final FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL, bundle);
 	     Pane root = fxmlLoader.load();
