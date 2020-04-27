47bbbe569c8b59cb5ed19baa0e145838891ba622
JunkJumper <jose.srifi@gmail.com>
2020-04-27 15:42:58 +0200

    script sql #4

--- database.sql
+++ database.sql
@@ -1,3 +1,16 @@
+-- Database: ShadowHunterDatabase
+
+-- DROP DATABASE "ShadowHunterDatabase";
+
+CREATE DATABASE "ShadowHunterDatabase"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'French_France.1252'
+    LC_CTYPE = 'French_France.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
 --
 -- PostgreSQL database dump
 --
