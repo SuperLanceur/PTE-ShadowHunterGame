17f5d37c5e74e68480e296b178db5fd35a04a554
Chiara <chiara.relevat@oulook.fr>
2020-05-05 15:38:11 +0200

    generalisation lancer des, choisir joueur, choisir equipement

---
+++ src/ihm/controller/ChoisirEquipement.java
@@ -0,0 +1,24 @@
+package ihm.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import carte.Equipement;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+import main.Joueur;
+
+public class ChoisirEquipement implements Initializable{
+	@FXML private GridPane equipement;
+	
+	private Joueur joueurVole;
+	private Equipement equipementVole;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
