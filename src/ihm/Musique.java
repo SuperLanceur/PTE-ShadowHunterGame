fb161fec2c6977e14a876462db61b4dd77b405e2
Bastien NOEL <bastien.noel@hotmail.fr>
2020-05-06 17:10:07 +0200

    réglage du volume de la musique dans les paramètre et le menu pause

--- src/ihm/Musique.java
+++ src/ihm/Musique.java
@@ -6,25 +6,32 @@ import java.io.InputStream;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-
+import javax.sound.sampled.FloatControl;
 
 public class Musique {
 
 	public static Clip clip;
 	public static long clipTimePosition = 0;
+	
+	public static boolean musiqueEnCours = false;
+	static FloatControl volume ;
 
 	public static void playMusique(InputStream path) {
 
 		try {
 
 			InputStream musicPath = path;
-			
-			if (musicPath!=null) {
+
+			if (musicPath != null) {
 				InputStream bufferedIn = new BufferedInputStream(musicPath);
 
 				AudioInputStream audioInput = AudioSystem.getAudioInputStream(bufferedIn);
 				clip = AudioSystem.getClip();
 				clip.open(audioInput);
+
+				volume =  (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+				volume.setValue(-20.0f); // Reduit le volume de 20 decibels.
+
 				clip.start();
 				clip.loop(Clip.LOOP_CONTINUOUSLY);
 
@@ -45,6 +52,14 @@ public class Musique {
 	public static void resumeMusique(Clip clip) {
 		clip.setMicrosecondPosition(clipTimePosition);
 		clip.start();
+	}
+	
+	public static void volumeUp(Float valeur) {
+		volume.setValue(volume.getValue() +valeur);
+	}
+	
+	public static void volumeDown(Float valeur) {
+		volume.setValue(volume.getValue() -valeur);
 	}
 
 }
